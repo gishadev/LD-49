fa2f185a4da9780f158760bc2ec396d112c54e06
Gisha <spaceshhz@gmail.com>
2021-10-02 21:37:01 +0300

    add camera follow controller

---
+++ LD-49/Assets/_Project/Scripts/CameraFollowController.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+namespace Gisha.LD49
+{
+    public class CameraFollowController : MonoBehaviour
+    {
+        [SerializeField] private float _followSpeed;
+        [SerializeField] private Transform _target;
+
+        private void Update()
+        {
+            FollowTarget();
+        }
+
+        private void FollowTarget()
+        {
+            var newPosition = new Vector3(_target.transform.position.x, _target.transform.position.y,
+                transform.position.z);
+
+            transform.position = Vector3.Lerp(transform.position, newPosition, _followSpeed * Time.deltaTime);
+        }
+    }
+}
