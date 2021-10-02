7b7302475e7a068ec645c3137e598c8857bc6f4e
Gisha <spaceshhz@gmail.com>
2021-10-02 22:45:56 +0300

    place new houses with passenger spots

---
+++ LD-49/Assets/_Project/Scripts/PassengerSpot.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Gisha.LD49
+{
+    public class PassengerSpot : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
