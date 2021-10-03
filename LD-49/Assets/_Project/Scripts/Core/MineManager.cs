0d8abebb1043a2917d309d931cc9a64226db79ba
Gisha <spaceshhz@gmail.com>
2021-10-03 13:52:08 +0300

    add safe speed feedback for speedometer

--- LD-49/Assets/_Project/Scripts/Core/MineManager.cs
+++ LD-49/Assets/_Project/Scripts/Core/MineManager.cs
@@ -5,30 +5,34 @@ namespace Gisha.LD49.Core
 {
     public class MineManager : MonoBehaviour
     {
+        private static MineManager Instance { get; set; }
+
         [SerializeField] private TaxiController taxiController;
-        [SerializeField] private float minCalmSpeed;
+        [SerializeField] private float minSafeSpeed;
         [SerializeField] private float explodeDelayInSeconds;
+        public static bool IsSafe => Instance.TaxiSpeed > Instance.minSafeSpeed;
+
         private float TaxiSpeed => taxiController.ConvertedSpeed;
 
         private float _delay;
 
         private void Awake()
         {
+            Instance = this;
             _delay = explodeDelayInSeconds;
         }
 
         private void Update()
         {
-            if (TaxiSpeed < minCalmSpeed)
+            if (!IsSafe)
             {
                 _delay -= Time.deltaTime;
                 if (_delay < 0f)
                     Explode();
             }
+
             else
-            {
                 _delay = explodeDelayInSeconds;
-            }
         }
 
         private void Explode()
--- LD-49/Assets/_Project/Scripts/GUI/SpeedometerGUI.cs
+++ LD-49/Assets/_Project/Scripts/GUI/SpeedometerGUI.cs
@@ -8,6 +8,8 @@ namespace Gisha.LD49.GUI
     public class SpeedometerGUI : MonoBehaviour
     {
         [SerializeField] private TMP_Text _speedometerTMPText;
+        [SerializeField] private Color safeColor;
+        [SerializeField] private Color dangerColor;
 
         private TaxiController _controller;
 
@@ -19,6 +21,16 @@ namespace Gisha.LD49.GUI
         private void Update()
         {
             _speedometerTMPText.text = _controller.ConvertedSpeed.ToString();
+            
+            ApplyColor(MineManager.IsSafe);
+        }
+
+        private void ApplyColor(bool isSafe)
+        {
+            if (isSafe)
+                _speedometerTMPText.color = safeColor;
+            else
+                _speedometerTMPText.color = dangerColor;
         }
     }
 }
