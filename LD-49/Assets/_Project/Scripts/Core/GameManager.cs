0d7d05d17b08dca5a12fc952a3e6488dc34db067
Gisha <spaceshhz@gmail.com>
2021-10-04 16:34:08 +0300

    update level

--- LD-49/Assets/_Project/Scripts/Core/GameManager.cs
+++ LD-49/Assets/_Project/Scripts/Core/GameManager.cs
@@ -1,16 +1,11 @@
-using System;
-using System.Collections;
 using Gisha.Effects.Audio;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 namespace Gisha.LD49.Core
 {
     public class GameManager : MonoBehaviour
     {
         [SerializeField] private GameObject loseMenu;
-        
-        [SerializeField] private float reloadDelay = 2f;
         private static GameManager Instance { get; set; }
 
         private void Awake()
